bf6a0d260abfc4d15fff60f84cd54ac2e5868ff3
Olga Demina <miserylab.r6s@gmail.com>
2022-11-09 21:40:24 +0300

    init commit

---
+++ petstore_tests/utils/sessions.py
@@ -0,0 +1,9 @@
+__author__ = 'miserylab'
+
+import os
+from petstore_tests.utils.requests_helper import BaseSession
+
+
+def reqres_api() -> BaseSession:
+    api_url = os.getenv('reqres_api_url')
+    return BaseSession(base_url=api_url)
